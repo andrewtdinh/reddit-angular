d829f46db7570e7148a0a23ddc0d5234f27fc068
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-11 02:50:38 -0700

    add new routes and empty new files on front end

--- client/config/config.js
+++ client/config/config.js
@@ -8,5 +8,9 @@ angular.module('reddit')
   .state('home', {url: '/', templateUrl: '/views/general/home.html'})
   .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
+  .state('posts', {url: '/posts', templateUrl: '/views/posts/posts.html', abstract: true})
+  .state('posts.new', {url: '/new', templateUrl: '/views/posts/posts-new.html', controller: 'PostsNewCtrl'})
+  .state('posts.show', {url: '/{postId}', templateUrl: '/views/posts/posts-show.html', controller: 'PostsShowCtrl'})
+  .state('posts.list', {url: '/', templateUrl: '/views/posts/posts-list.html', controller: 'PostsListCtrl'})
   .state('dashboard', {url: '/dashboard', templateUrl: '/views/users/dashboard.html', controller: 'DashboardCtrl'});
 });
