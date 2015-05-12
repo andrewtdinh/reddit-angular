e8b93303c69eb14264b61af021334f65f2b451e0
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-12 07:38:19 -0700

    Click on list of posts can go either to internal state or external url

--- client/views/posts/posts-list.js
+++ client/views/posts/posts-list.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('reddit')
-.controller('PostsListCtrl', function($scope, Post){
+.controller('PostsListCtrl', function($scope, Post, $state, $window){
   Post.show()
   .then(function(response){
-    console.info('response: ', response);
     $scope.posts = response.data;
   });
+  $scope.show = function(post){
+    if(post.url){
+      $window.location.href = post.url;
+    } else {
+      $state.go('posts.show', {postId: post._id});
+    }
+  };
 });
